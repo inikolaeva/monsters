206278f6180eeeb67908847ca1d59a300edd32df
inikolaeva <ityurinka@gmail.com>
2020-05-17 13:44:40 +0300

    Updates

---
+++ precache-manifest.ee74646aa70da723c8d7b9f40d55c46e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23f95b2287d8a0e30216582cd2626b2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "74846623b0d87502fdf4",
+    "url": "/monsters/static/css/main.ea0647b5.chunk.css"
+  },
+  {
+    "revision": "51f6a86ff27fff47c0c6",
+    "url": "/monsters/static/js/2.d7536941.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters/static/js/2.d7536941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74846623b0d87502fdf4",
+    "url": "/monsters/static/js/main.977224d2.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
